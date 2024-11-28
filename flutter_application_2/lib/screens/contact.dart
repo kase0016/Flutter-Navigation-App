22c9edb4a212461bdff77562aacc26508ebdb8cf
Jean-Baptiste Kasenda <kase0016@algonquinlive.com>
2024-11-27 19:30:38 -0500

    fixing styling on form

--- flutter_application_2/lib/screens/contact.dart
+++ flutter_application_2/lib/screens/contact.dart
@@ -9,59 +9,64 @@ class ContactPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Form(
-                autovalidateMode: AutovalidateMode.always,
-                key: _formStateKey,
-                child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(children: <Widget>[
-                      TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Name',
-                          ),
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter a valid name.';
-                            }
-                            return null;
-                          },
-                          onSaved: (String? value) {
-                            _data.name = value!;
-                          }),
-                      TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email',
-                          ),
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter a valid email.';
-                            }
-                            return null;
-                          },
-                          onSaved: (String? value) {
-                            _data.email = value!;
-                          }),
-                      TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Message',
-                          ),
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                          onSaved: (String? value) {
-                            _data.message = value!;
-                          }),
-                      TextButton(
-                          child: const Text('Send'),
-                          onPressed: () {
-                            if (_formStateKey.currentState!.validate()) {
-                              _formStateKey.currentState?.save();
-                            } else {}
-                          })
-                    ])))));
+      child: Center(
+          child: Form(
+              autovalidateMode: AutovalidateMode.always,
+              key: _formStateKey,
+              child: Padding(
+                  padding: const EdgeInsets.all(35.0),
+                  child: Column(children: <Widget>[
+                    Text("We Want To Hear From You",
+                        style: TextStyle(fontSize: 45, height: 1)),
+                    TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          labelText: 'Name',
+                        ),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a valid name.';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          _data.name = value!;
+                        }),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                        ),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a valid email.';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          _data.email = value!;
+                        }),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Message',
+                        ),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          _data.message = value!;
+                        }),
+                    TextButton(
+                        child: const Text('Send'),
+                        onPressed: () {
+                          if (_formStateKey.currentState!.validate()) {
+                            _formStateKey.currentState?.save();
+                          } else {}
+                        })
+                  ])))),
+    ));
   }
 }
 
